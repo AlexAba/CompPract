c6935fcfd37b6d7441ed88bdaaa65db6e13a2dc5
AlexAba <alekseiabakumenko@gmail.com>
2020-04-19 17:11:04 +0300

    Fixed incorrect fermi velocity value for InSb

--- constants/InSb.py
+++ constants/InSb.py
@@ -4,5 +4,5 @@ class InSb:
     id = 'InSb'             #   Semiconductor type/name
     elMob = 7.7             #   Electron mobility
     efMass = 0.014 * m_e    #   Effective mass 
-    fermVel = 3.1e5         #   Fermi velocity
+    fermVel = 9.8e5         #   Fermi velocity
     
